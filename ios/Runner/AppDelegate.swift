39883f9fde6d7b9833e7b58dedbeb32abc854621
chenarrr <chenar.breshk@gmail.com>
2026-05-14 14:39:26 +0300

    connecting to a real iphone

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,6 +8,7 @@ import FirebaseCore
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    FirebaseApp.configure()
     FirebaseConfiguration.shared.setLoggerLevel(.min)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
